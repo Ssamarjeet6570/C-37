be62bcb6d39d4ef1a9dadc40cff5ed4e2c4aab22
Ssamarjeet6570 <ssamarjeet6570@gmail.com>
2021-02-13 15:59:51 +0530

    changes

--- sketch.js
+++ sketch.js
@@ -4,12 +4,12 @@ var gameState = 0;
 var playerCount;
 
 var database;
-
+var allPlayers,distance = 0;
 var form, player, game;
 
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
